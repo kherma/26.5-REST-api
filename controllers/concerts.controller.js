1aba062eb9b0651cb2dc9d49bac623133c8fcb68
kherma <kamil.herma01@gmail.com>
2026-03-29 17:06:05 +0200

    refactor: extract concerts controller from routes

---
+++ controllers/concerts.controller.js
@@ -0,0 +1,111 @@
+const Concert = require('../models/Concert');
+
+const getAllConcerts = async (req, res) => {
+  try {
+    const concerts = await Concert.find();
+    return res.json(concerts);
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const getConcertById = async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const concert = await Concert.findOne({ id });
+
+    if (!concert) {
+      return res.status(404).json({ message: 'Concert not found' });
+    }
+
+    return res.json(concert);
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const createConcert = async (req, res) => {
+  try {
+    const { performer, genre, price, day, image } = req.body;
+    const priceNumber = Number(price);
+    const dayNumber = Number(day);
+
+    if (!performer || !genre || !price || !day || !image) {
+      return res
+        .status(400)
+        .json({ message: 'performer, genre, price, day and image required' });
+    }
+
+    const lastConcert = await Concert.findOne().sort({ id: -1 }).select('id -_id');
+    const newId = lastConcert ? lastConcert.id + 1 : 1;
+    const newConcert = new Concert({
+      id: newId,
+      performer,
+      genre,
+      price: priceNumber,
+      day: dayNumber,
+      image,
+    });
+
+    await newConcert.save();
+
+    return res.json({ message: 'OK' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const updateConcert = async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const { performer, genre, price, day, image } = req.body;
+    const priceNumber = Number(price);
+    const dayNumber = Number(day);
+    const concert = await Concert.findOne({ id });
+
+    if (!concert) {
+      return res.status(404).json({ message: 'Concert not found' });
+    }
+
+    if (!performer || !genre || !price || !day || !image) {
+      return res
+        .status(400)
+        .json({ message: 'performer, genre, price, day and image required' });
+    }
+
+    await Concert.findByIdAndUpdate(concert._id, {
+      performer,
+      genre,
+      price: priceNumber,
+      day: dayNumber,
+      image,
+    });
+
+    return res.json({ message: 'OK' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const deleteConcert = async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const deletedConcert = await Concert.findOneAndDelete({ id });
+
+    if (!deletedConcert) {
+      return res.status(404).json({ message: 'Concert not found' });
+    }
+
+    return res.json({ message: 'OK' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+module.exports = {
+  getAllConcerts,
+  getConcertById,
+  createConcert,
+  updateConcert,
+  deleteConcert,
+};
--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -1,108 +1,22 @@
 const express = require('express');
-const Concert = require('../models/Concert');
+const {
+  getAllConcerts,
+  getConcertById,
+  createConcert,
+  updateConcert,
+  deleteConcert,
+} = require('../controllers/concerts.controller');
 
 const router = express.Router();
 
-router.get('/concerts', async (req, res) => {
-  try {
-    const concerts = await Concert.find();
-    return res.json(concerts);
-  } catch (error) {
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-});
+router.get('/concerts', getAllConcerts);
 
-router.get('/concerts/:id', async (req, res) => {
-  try {
-    const id = Number(req.params.id);
-    const concert = await Concert.findOne({ id });
+router.get('/concerts/:id', getConcertById);
 
-    if (!concert) {
-      return res.status(404).json({ message: 'Concert not found' });
-    }
+router.post('/concerts', createConcert);
 
-    return res.json(concert);
-  } catch (error) {
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-});
+router.put('/concerts/:id', updateConcert);
 
-router.post('/concerts', async (req, res) => {
-  try {
-    const { performer, genre, price, day, image } = req.body;
-    const priceNumber = Number(price);
-    const dayNumber = Number(day);
-
-    if (!performer || !genre || !price || !day || !image) {
-      return res
-        .status(400)
-        .json({ message: 'performer, genre, price, day and image required' });
-    }
-
-    const lastConcert = await Concert.findOne().sort({ id: -1 }).select('id -_id');
-    const newId = lastConcert ? lastConcert.id + 1 : 1;
-    const newConcert = new Concert({
-      id: newId,
-      performer,
-      genre,
-      price: priceNumber,
-      day: dayNumber,
-      image,
-    });
-
-    await newConcert.save();
-
-    return res.json({ message: 'OK' });
-  } catch (error) {
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-router.put('/concerts/:id', async (req, res) => {
-  try {
-    const id = Number(req.params.id);
-    const { performer, genre, price, day, image } = req.body;
-    const priceNumber = Number(price);
-    const dayNumber = Number(day);
-    const concert = await Concert.findOne({ id });
-
-    if (!concert) {
-      return res.status(404).json({ message: 'Concert not found' });
-    }
-
-    if (!performer || !genre || !price || !day || !image) {
-      return res
-        .status(400)
-        .json({ message: 'performer, genre, price, day and image required' });
-    }
-
-    await Concert.findByIdAndUpdate(concert._id, {
-      performer,
-      genre,
-      price: priceNumber,
-      day: dayNumber,
-      image,
-    });
-
-    return res.json({ message: 'OK' });
-  } catch (error) {
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-router.delete('/concerts/:id', async (req, res) => {
-  try {
-    const id = Number(req.params.id);
-    const deletedConcert = await Concert.findOneAndDelete({ id });
-
-    if (!deletedConcert) {
-      return res.status(404).json({ message: 'Concert not found' });
-    }
-
-    return res.json({ message: 'OK' });
-  } catch (error) {
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-});
+router.delete('/concerts/:id', deleteConcert);
 
 module.exports = router;
